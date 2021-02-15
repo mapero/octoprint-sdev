e723c9922022477d4db1a1b8fcd7e9a53484ae39
Jochen <webmaster@mapero.de>
2021-02-15 21:44:09 +0100

    chg: minor improvements

--- wrapper/bin/generate_manifest.sh
+++ wrapper/bin/generate_manifest.sh
@@ -2,6 +2,6 @@
 
 # Update json
 PORT=$(snapctl get port)
-mkdir -p $SNAP_DATA/package-assets/$SNAP_INSTANCE_NAME
+mkdir -p $SNAP_DATA/package-assets/$SNAP_NAME
 jq ".menus.sidebar[].link = \"http://\${hostname}:$PORT\" | .menus.sidebar[].title = \"Octoprint $SNAP_INSTANCE_KEY\" | .menus.sidebar[].id = \"octoprint-sdev.octoprint.$SNAP_INSTANCE_KEY\"" \
-    $SNAP/package-assets/octoprint-sdev/octoprint-sdev.package-manifest.json > $SNAP_DATA/package-assets/$SNAP_INSTANCE_NAME/$SNAP_INSTANCE_NAME.package-manifest.json
+    $SNAP/package-assets/octoprint-sdev/octoprint-sdev.package-manifest.json > $SNAP_DATA/package-assets/octoprint-sdev/$SNAP_INSTANCE_NAME.package-manifest.json
